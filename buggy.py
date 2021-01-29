5e519411356d43c4298bbaebd637a167468f5f8a
Kyle Brown <56103801+KyleBrown-804@users.noreply.github.com>
2021-01-28 21:47:50 -0800

    Migration to Flask and Python Environment

---
+++ buggy.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, jsonify, request, redirect, flash, url_for
+from werkzeug.utils import secure_filename
+from loadH5 import runPrediction
+import os
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'static/uploads'
+
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
+@app.route('/', methods=["GET", "POST"])
+def upload_file():
+
+    if request.method == 'POST':
+    
+        if request.files:
+            image = request.files['inputFile']
+            filename = secure_filename(image.filename)
+
+            print(filename)
+
+            image.save(os.path.join(app.config['UPLOAD_FOLDER'], image.filename))
+
+            # runPrediction(image.filename)
+            return redirect(request.url)
+
+    return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ loadH5.py
@@ -0,0 +1,26 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.resnet50 import preprocess_input, decode_predictions
+import os
+import numpy as np
+
+def runPrediction(img_name):
+    
+    print('Loading model...')
+    trained_model = tf.keras.models.load_model('models/resnet_test.h5')
+    print('Model loaded successfully')
+
+    img_path = 'static/uploads/' + img_name
+
+    img = image.load_img(img_path, target_size=(224, 224))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+
+    preds = trained_model.predict(x)
+    # decode the results into a list of tuples (class, description, probability)
+    # (one such list for each sample in the batch)
+    print('Predicted:', decode_predictions(preds, top=3)[0])
+
+    # trained_model.summary()
