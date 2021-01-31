872aaa316a98db2db696994a1ca02f4630ddb181
Miguel Garnica <mgarnica123@aol.com>
2021-01-30 17:19:22 -0800

    load model super hotfix
    fix the prediction method (now predicts without crashing 'the zucc 79.99% confirmed an aphid')

--- loadH5.py
+++ loadH5.py
@@ -4,26 +4,45 @@ from tensorflow.keras.preprocessing import image
 from tensorflow.keras.applications.resnet50 import preprocess_input, decode_predictions
 import os
 import numpy as np
+import pathlib
 
 def runPrediction(img_name):
-    
     print('Loading model...')
-    trained_model = tf.keras.models.load_model('models/buggyAI_alpha.h5')
+    model = tf.keras.models.load_model('models/buggyAI_alpha.h5')
     print('Model loaded successfully')
 
-    #img_path = 'static/uploads/' + img_name
+        #img_path = 'static/uploads/' + img_name
+    img_height = 180
+    img_width = 180
 
-    img = image.load_img(img_name, target_size=(180, 180))
-    x = image.img_to_array(img)
-    x = np.expand_dims(x, axis=0)
-    x = preprocess_input(x)
+    class_names = ['aphids', 'red-spider-mites', 'thrips']
 
-    preds = trained_model.predict(x)
-    # decode the results into a list of tuples (class, description, probability)
-    # (one such list for each sample in the batch)
-    print('Predicted:', decode_predictions(preds, top=1)[0])
+    test_data = img_name
+    test_dir = pathlib.Path(test_data)
 
+    img = keras.preprocessing.image.load_img(
+        test_dir, target_size=(img_height, img_width)
+    )
+    img_array = keras.preprocessing.image.img_to_array(img)
+    img_array = tf.expand_dims(img_array, 0) # Create a batch
+
+    predictions = model.predict(img_array)
+    score = tf.nn.softmax(predictions[0])
+        # decode the results into a list of tuples (class, description, probability)
+        # (one such list for each sample in the batch)
+    print(
+        "This image most likely belongs to {} with a {:.2f} percent confidence."
+        .format(class_names[np.argmax(score)], 100 * np.max(score))
+    )
+
+
+    print("donezoooo")
+
+
+        
+"""
     if os.path.exists(os.path.join(os.getcwd(), img_name)):
         os.remove(os.path.join(os.getcwd(), img_name))
 
     # trained_model.summary()
+"""
