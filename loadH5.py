5c968381f381a0a892afa600c9d67c2870139df3
Miguel Garnica <mgarnica123@aol.com>
2021-01-30 16:30:36 -0800

    BuggyAI Classification Model Alpha
    Added:
        - buggyAI_alpha.h5 (saved h5 model )
    Updated:
        - loadH5.py (use newly trained buggy model)
     Removed:
        - mobilenetv2.h5 model

--- loadH5.py
+++ loadH5.py
@@ -8,7 +8,7 @@ import numpy as np
 def runPrediction(img_name):
     
     print('Loading model...')
-    trained_model = tf.keras.models.load_model('models/mobilenetv2.h5')
+    trained_model = tf.keras.models.load_model('models/buggyAI_alpha.h5')
     print('Model loaded successfully')
 
     #img_path = 'static/uploads/' + img_name
@@ -21,7 +21,7 @@ def runPrediction(img_name):
     preds = trained_model.predict(x)
     # decode the results into a list of tuples (class, description, probability)
     # (one such list for each sample in the batch)
-    print('Predicted:', decode_predictions(preds, top=3)[0])
+    print('Predicted:', decode_predictions(preds, top=1)[0])
 
     if os.path.exists(os.path.join(os.getcwd(), img_name)):
         os.remove(os.path.join(os.getcwd(), img_name))
